6f47a0d58a09e1d3d67736c9c273ec05d526fe94
utay4to <www.yy42s6.www@gmail.com>
2022-06-29 14:08:07 +0000

    set timezone

--- message-board/resources/views/welcome.blade.php
+++ message-board/resources/views/welcome.blade.php
@@ -71,19 +71,19 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-
+    
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
                 </div>
             @endif
-
+    
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    太郎
                 </div>
-
+    
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
